3aae4f770ddfbf9445e7c4d610878ee6b8a1e3a4
SarahhhC <joytutu29@naver.com>
2016-08-11 01:00:10 +0900

    heroku

--- gulpfile.js
+++ gulpfile.js
@@ -28,7 +28,7 @@ gulp.task('startserver', function() {
 });
 
 gulp.task('openbrowser', function() {
-    return opn('http://localhost:3000');
+    return opn('https://stark-shelf-48078.herokuapp.com');
 });
 
 gulp.task('default', function() {
--- src/client/Main.js
+++ src/client/Main.js
@@ -10,4 +10,14 @@ var _FacebookButton2 = _interopRequireDefault(_FacebookButton);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-_react2.default.render(_react2.default.createElement(_FacebookButton2.default, { fb: FB }), document.getElementById('facebook-login'));
+var HelloWorld = _react2.default.createClass({
+  displayName: 'HelloWorld',
+
+  render: function render() {
+    return _react2.default.createElement(_FacebookButton2.default, { fb: FB });
+  }
+});
+
+setInterval(function () {
+  ReactDOM.render(_react2.default.createElement(HelloWorld, { date: new Date() }), document.getElementById('fb-login-button'));
+}, 500);
