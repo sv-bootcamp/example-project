fc542102d1d08a2e74e87ef14d23020a2f4c3207
cel <yangwoo@celui-MacBook-Pro.local>
2016-08-11 14:44:00 +0900

    make it better

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var webpack = require('webpack')
-var webpackDevMiddleware = require('webpack-dev-middleware')
-var webpackHotMiddleware = require('webpack-hot-middleware')
+// var webpackDevMiddleware = require('webpack-dev-middleware')
+// var webpackHotMiddleware = require('webpack-hot-middleware')
 var bodyParser = require("body-parser")
 var config = require('./webpack.config')
 var request = require('request')
@@ -10,13 +10,13 @@ var ejs = require('ejs')
 var app = new(require('express'))()
 var port = 3000
 
-var compiler = webpack(config)
-app.use(webpackDevMiddleware(compiler, {
-	noInfo: true,
-	publicPath: config.output.publicPath
-}))
-app.set('view engine', 'ejs')
-app.use(webpackHotMiddleware(compiler))
+// var compiler = webpack(config)
+// app.use(webpackDevMiddleware(compiler, {
+// 	noInfo: true,
+// 	publicPath: config.output.publicPath
+// }))
+// app.set('view engine', 'ejs')
+// app.use(webpackHotMiddleware(compiler))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
