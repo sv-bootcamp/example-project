cf8c9263a945d0af4d95ad7e4e155346aaebe026
MechanicKim <mechanic0406@gmail.com>
2016-08-11 17:44:52 +0900

    Update fblogin

---
+++ src/server/fblogin/routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'FB Login Counter' });
+});
+
+module.exports = router;
