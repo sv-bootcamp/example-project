9159aa79f5a1ee45e1105a38faad1428cb124dda
MechanicKim <mechanic0406@gmail.com>
2016-08-12 13:30:29 +0900

    solve tab problem

--- src/client/FacebookLogin/index.android.js
+++ src/client/FacebookLogin/index.android.js
@@ -1,51 +1,44 @@
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  View,
+	AppRegistry,
+	StyleSheet,
+	View,
 	Alert,
 	Networking
 } from 'react-native';
 
 const FBSDK = require('react-native-fbsdk');
 const {
-  LoginButton,
+	LoginButton,
 	AccessToken
 } = FBSDK;
 
 var FacebookLogin = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <LoginButton
-          onLoginFinished={
-            (error, result) => {
+	render: function() {
+		return (
+			<View style = {styles.container}>
+				<LoginButton
+					onLoginFinished = {
+						(error, result) => {
 							if (error) {
-								Alert.alert('알림',	'문제발생!',
-									[{text: '확인', onPress: () => console.log('OK Pressed')}]
-								);
-              } else if (result.isCancelled) {
-                Alert.alert('알림',	'로그인이 취소됨.',
-									[{text: '확인', onPress: () => console.log('OK Pressed')}]
-								);
-              } else {
+								Alert.alert('알림',	'문제발생!', [{text: '확인', onPress: () => console.log('OK Pressed')}]);
+							} else if (result.isCancelled) {
+								Alert.alert('알림',	'로그인이 취소됨.', [{text: '확인', onPress: () => console.log('OK Pressed')}]);
+							} else {
 								this.getToken();
-              }
-            }
-          }
-          onLogoutFinished={
-						() => {
-							Alert.alert(
-								'알림',
-								'로그아웃 되었습니다.',
-								[{text: '확인', onPress: () => console.log('OK Pressed')}]
-							)
+							}
 						}
-					}/>
-      </View>
-    );
-  },
-	
+					}
+					onLogoutFinished={
+						() => {
+							Alert.alert('알림', '로그아웃 되었습니다.', [{text: '확인', onPress: () => console.log('OK Pressed')}])
+						}
+					}
+				/>
+			</View>
+		);
+	},
+
 	getToken: function() {
 		AccessToken.getCurrentAccessToken().then(
 			(data) => {
@@ -54,7 +47,7 @@ var FacebookLogin = React.createClass({
 			}
 		);
 	},
-	
+
 	getUserInfo: function(token) {		
 		fetch('https://graph.facebook.com/me?access_token=' + token)
 		.then((response) => response.json())
@@ -66,7 +59,7 @@ var FacebookLogin = React.createClass({
 			console.error(error);
 		});
 	},
-	
+
 	getLoginCount: function(token, userName) {
 		fetch('https://fblogincounterdemo.herokuapp.com/login?token=' + token)
 		.then((response) => response.json())
@@ -85,12 +78,12 @@ var FacebookLogin = React.createClass({
 });
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  }
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF'
+	}
 });
 
 AppRegistry.registerComponent('FacebookLogin', () => FacebookLogin);
